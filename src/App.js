4ababff93ce8625c9cf25f142835d43ff195b7a6
PranayChavhan <pranay21chavhan@outlook.com>
2022-01-08 14:01:02 +0530

    Added date and author and source

--- src/App.js
+++ src/App.js
@@ -2,14 +2,59 @@ import "./App.css";
 import Navbar from "./components/Navbar.js";
 import React, { Component } from "react";
 import News from "./components/News";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar />
-        <News />
-      </div>
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+          
+          <Route
+            path="/"
+            element={
+              <News key="general" country="in" size="9" category="general" />
+            }
+          />
+          <Route
+            path="/business"
+            element={
+              <News key="business" country="in" size="9" category="business" />
+            }
+          />
+          <Route
+            path="/entertainment"
+            element={
+              <News key="entertainment" country="in" size="9" category="entertainment" />
+            }
+          />
+          <Route
+            path="/health"
+            element={
+              <News key="health" country="in" size="9" category="health" />
+            }
+          />
+          <Route
+            path="/science"
+            element={
+              <News key="science" country="in" size="9" category="science" />
+            }
+          />
+          <Route
+            path="/sports"
+            element={
+              <News key="sports" country="in" size="9" category="sports" />
+            }
+          />
+          <Route
+            path="/technology"
+            element={
+              <News key="technology" country="in" size="9" category="technology" />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     );
   }
 }
--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -2,15 +2,34 @@ import React, { Component } from "react";
 
 export default class NewsItems extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl} = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
     return (
       <div className="my-3">
-        <div className="card" style={{ width: "18rem" }}>
+        <div className="card">
           <img src={imageUrl} className="card-img-top" alt="..." />
+
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
+            <span
+              className="position-absolute   translate-middle badge rounded-pill bg-danger"
+              style={{ left: "80%", zIndex: "1" }}
+            >
+              {source}
+            </span>
+            <h5 className="card-title mt-3">{title}...</h5>
             <p className="card-text">{description}...</p>
-            <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">
+            <p className="card-text">
+              <small className="text-muted">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-sm btn-dark"
+            >
               Read more
             </a>
           </div>
